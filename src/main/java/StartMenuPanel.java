328066e508369a03e74fed2c9a44a817a9e4acfc
SDaniD <danidds2509d@gmail.com>
2023-10-15 22:10:33 +0200

    menu

--- src/main/java/StartMenuPanel.java
+++ src/main/java/StartMenuPanel.java
@@ -11,56 +11,55 @@ public class StartMenuPanel extends JPanel {
     private JButton start;
     private JButton levels;
     private JButton exit;
+    private JLabel buttonsPanel;
 
     public StartMenuPanel(MarioFrame frame) {
         super.setFocusable(true);
         super.setVisible(true);
         super.setBackground(Color.green);
-        frame.setLayout(new GridLayout());
+        super.setLayout(new BorderLayout(30,40));
 
         try {
-            mario = Font.createFont(Font.TRUETYPE_FONT, new File("./resources/fonts/SuperMario256.ttf")).deriveFont(50);
+            mario = Font.createFont(Font.TRUETYPE_FONT, new File("./resources/fonts/SuperMario256.ttf"));
         } catch (IOException e) {
             System.out.println("No font found");
         } catch (FontFormatException e) {
             System.out.println("Wrong font format");
         }
 
+        buttonsPanel = new JLabel();
+        buttonsPanel.setOpaque(true);
+        buttonsPanel.setSize(500, 400);
+        buttonsPanel.setLayout(new SpringLayout());
+        buttonsPanel.setLocation(960, 350);
+        buttonsPanel.setBackground(Color.orange);
+        frame.add(buttonsPanel);
 
         title = new JLabel();
         title.setText("TU/e Mario");
-        title.setFont(mario);
-        title.setMinimumSize(new Dimension(50,20));
-        title.setLocation(frame.getWidth()/2, 10);
-        super.add(title);
-        title.setVisible(true);
+        title.setFont(mario.deriveFont(50f));
+        super.add(title, BorderLayout.PAGE_START);
 
-        /*start = new JButton();
+        start = new JButton();
         start.setIcon(new ImageIcon("./resources/ui-elements/img.png"));
+        start.setMaximumSize(new Dimension(5,2));
         start.setText("START");
-        start.setFont(mario.deriveFont(20));
-        start.setLocation(frame.getWidth()-200, frame.getHeight()/2);
-        start.setSize(50,20);
-        super.add(start);
-        start.setVisible(true);
+        start.setFont(mario.deriveFont(20f));
+        start.setSize(5,2);
+        buttonsPanel.add(start);
 
-        levels = new JButton();
+        /*levels = new JButton();
         levels.setIcon(new ImageIcon("./resources/ui-elements/img.png"));
         levels.setText("LEVELS");
-        levels.setFont(mario.deriveFont(20));
-        levels.setLocation(frame.getWidth()-200, frame.getHeight()/2+70);
-        levels.setSize(50,20);
-        super.add(levels);
-*/
+        levels.setFont(mario.deriveFont(20f));
+        levels.setSize(5,2);
+        buttonsPanel.add(levels);
+
         exit = new JButton();
         exit.setIcon(new ImageIcon("./resources/ui-elements/img.png"));
         exit.setText("EXIT");
-        exit.setFont(mario);
-        exit.setLocation(frame.getWidth()-200, frame.getHeight()/2+140);
-        exit.setSize(50,20);
-        super.add(exit);
-
-
-
+        exit.setFont(mario.deriveFont(20f));
+        exit.setSize(5,2);
+        buttonsPanel.add(exit);*/
     }
 }
